d21b0dd78a9d70c213fd7c66d6372aee6c771218
“gstkrsnaprnta” <gstkrisnapranata@gmail.com>
2024-10-13 11:10:18 +0800

    feat: create auth remote datasource

---
+++ lib/data/datasources/auth_remote_datasource.dart
@@ -0,0 +1,22 @@
+import 'package:aplikasi_sipos/core/constants/variables.dart';
+import 'package:aplikasi_sipos/data/model/response/auth_response_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
+
+class AuthRemoteDatasource {
+  Future<Either<String, AuthResponseModel>> login(
+    String email,
+    String password,
+  ) async {
+    final response =
+        await http.post(Uri.parse('${Variables.baseUrl}/api/login'), body: {
+      'email': email,
+      'password': password,
+    });
+    if (response.statusCode == 200) {
+      return right(AuthResponseModel.fromJson(response.body as Map<String, dynamic>));
+    } else {
+      return left(response.body);
+    }
+  }
+}
