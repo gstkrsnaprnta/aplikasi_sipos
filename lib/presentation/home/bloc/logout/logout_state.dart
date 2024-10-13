8ffc0c83ec6f2735fcc6f1b89acba47404f2b956
“gstkrsnaprnta” <gstkrisnapranata@gmail.com>
2024-10-13 19:29:10 +0800

    chore: remove comment

--- lib/presentation/home/bloc/logout/logout_state.dart
+++ lib/presentation/home/bloc/logout/logout_state.dart
@@ -5,6 +5,5 @@ class LogoutState with _$LogoutState {
   const factory LogoutState.initial() = _Initial;
   const factory LogoutState.loading() = _Loading;
   const factory LogoutState.success() = _Success;
-  const factory LogoutState.error(String message) =
-      _Error; // Misalnya menambahkan pesan error
+  const factory LogoutState.error(String message) = _Error;
 }
