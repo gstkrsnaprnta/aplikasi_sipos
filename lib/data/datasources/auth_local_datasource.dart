a32cd668ac2ee4955f5406f165d3d04d252dd0d6
“gstkrsnaprnta” <gstkrisnapranata@gmail.com>
2024-10-13 15:30:57 +0800

    feat: create auth local datasource

---
+++ lib/data/datasources/auth_local_datasource.dart
@@ -0,0 +1,30 @@
+import 'dart:convert'; // Tambahkan ini untuk jsonEncode dan jsonDecode
+import 'package:aplikasi_sipos/data/model/response/auth_response_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthLocalDatasource {
+  Future<void> saveAuthData(AuthResponseModel authResponseModel) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('auth_data', jsonEncode(authResponseModel.toJson()));
+  }
+
+  Future<void> removeAuthData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('auth_data');
+  }
+
+  Future<AuthResponseModel> getAuthData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final authData = prefs.getString('auth_data');
+
+    return AuthResponseModel.fromJson(jsonDecode(authData!));
+  }
+
+  Future<bool> isAuth() async {
+    final prefs = await SharedPreferences.getInstance();
+    final authData = prefs.getString('auth_data');
+
+    return authData != null;
+  }
+}
