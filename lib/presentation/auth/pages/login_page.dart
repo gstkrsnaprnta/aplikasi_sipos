c6cffd4924ba1e8c7c25ccc408d81e417862dc4c
“gstkrsnaprnta” <gstkrisnapranata@gmail.com>
2024-10-13 14:53:22 +0800

    feat: implent bloc in login page

--- lib/presentation/auth/pages/login_page.dart
+++ lib/presentation/auth/pages/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:aplikasi_sipos/presentation/auth/bloc/login/login_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/assets/assets.gen.dart';
 import '../../../core/components/buttons.dart';
@@ -51,16 +53,47 @@ class _LoginPageState extends State<LoginPage> {
             obscureText: true,
           ),
           const SpaceHeight(24.0),
-          Button.filled(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const DashboardPage(),
-                ),
+          BlocListener<LoginBloc, LoginState>(
+            listener: (context, state) {
+              state.maybeWhen(
+                orElse: () {},
+                success: (authResponseModel) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DashboardPage()),
+                  );
+                },
+                error: (message) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(message),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                },
               );
             },
-            label: 'Masuk',
+            child: BlocBuilder<LoginBloc, LoginState>(
+              builder: (context, state) {
+                return state.maybeWhen(
+                  orElse: () {
+                    return Button.filled(
+                      onPressed: () {
+                        context.read<LoginBloc>().add(LoginEvent.login(
+                            email: usernameController.text,
+                            password: passwordController.text));
+                      },
+                      label: 'Masuk',
+                    );
+                  },
+                  loading: () {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ],
       ),
