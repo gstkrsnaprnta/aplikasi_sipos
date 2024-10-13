d9aa7609993a265d09dc298fe9b6773f836df6df
“gstkrsnaprnta” <gstkrisnapranata@gmail.com>
2024-10-13 17:05:03 +0800

    feat: add bloc cosumer for logout bloc

--- lib/presentation/home/pages/dashboard_page.dart
+++ lib/presentation/home/pages/dashboard_page.dart
@@ -1,4 +1,8 @@
+import 'package:aplikasi_sipos/data/datasources/auth_local_datasource.dart';
+import 'package:aplikasi_sipos/presentation/auth/pages/login_page.dart';
+import 'package:aplikasi_sipos/presentation/home/bloc/logout/logout_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -13,6 +17,31 @@ class _DashboardPageState extends State<DashboardPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dashboard"),
+        actions: [
+          BlocConsumer<LogoutBloc, LogoutState>(
+            listener: (context, state) {
+              state.maybeWhen(
+                orElse: () {},
+                success: () {
+                  AuthLocalDatasource().removeAuthData();
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => LoginPage()));
+                },
+              );
+            },
+            builder: (context, state) {
+              return IconButton(
+                onPressed: () {
+                  context.read<LogoutBloc>().add(LogoutEvent.logout());
+                },
+                icon: const Icon(
+                  Icons.add,
+                  size: 24.0,
+                ),
+              );
+            },
+          ),
+        ],
         automaticallyImplyLeading: false, // Menghilangkan tombol back
       ),
       body: Center(
