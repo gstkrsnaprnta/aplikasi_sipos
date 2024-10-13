d5c126f3e58bd2133228288f833df2ca85bed0d3
“gstkrsnaprnta” <gstkrisnapranata@gmail.com>
2024-10-13 22:44:51 +0800

    feat: create home page

--- lib/presentation/home/pages/home_page.dart
+++ lib/presentation/home/pages/home_page.dart
@@ -1,13 +1,14 @@
+import 'package:aplikasi_sipos/presentation/auth/pages/login_page.dart';
+import 'package:aplikasi_sipos/presentation/home/bloc/product/product_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/assets/assets.gen.dart';
 import '../../../core/components/menu_button.dart';
 import '../../../core/components/search_input.dart';
 import '../../../core/components/spaces.dart';
-import '../models/product_category.dart';
-import '../models/product_model.dart';
 import '../widgets/product_empty.dart';
-import 'product_card.dart';
+import '../widgets/product_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,62 +21,75 @@ class _HomePageState extends State<HomePage> {
   final searchController = TextEditingController();
   final indexValue = ValueNotifier(0);
 
-  List<ProductModel> searchResults = [];
-  final List<ProductModel> products = [
-    ProductModel(
-      image: Assets.images.f1.path,
-      name: 'Nutty Latte',
-      category: ProductCategory.drink,
-      price: 39000,
-      stock: 10,
-    ),
-    ProductModel(
-      image: Assets.images.f2.path,
-      name: 'Iced Latte',
-      category: ProductCategory.drink,
-      price: 24000,
-      stock: 10,
-    ),
-    ProductModel(
-      image: Assets.images.f3.path,
-      name: 'Iced Mocha',
-      category: ProductCategory.drink,
-      price: 33000,
-      stock: 10,
-    ),
-    ProductModel(
-      image: Assets.images.f4.path,
-      name: 'Hot Mocha',
-      category: ProductCategory.drink,
-      price: 33000,
-      stock: 10,
-    ),
-  ];
+  // List<ProductModel> searchResults = [];
+  // final List<ProductModel> products = [
+  //   ProductModel(
+  //     image: Assets.images.f1.path,
+  //     name: 'Nutty Latte',
+  //     category: ProductCategory.drink,
+  //     price: 39000,
+  //     stock: 10,
+  //   ),
+  //   ProductModel(
+  //     image: Assets.images.f2.path,
+  //     name: 'Iced Latte',
+  //     category: ProductCategory.drink,
+  //     price: 24000,
+  //     stock: 10,
+  //   ),
+  //   ProductModel(
+  //     image: Assets.images.f3.path,
+  //     name: 'Iced Mocha',
+  //     category: ProductCategory.drink,
+  //     price: 33000,
+  //     stock: 10,
+  //   ),
+  //   ProductModel(
+  //     image: Assets.images.f4.path,
+  //     name: 'Hot Mocha',
+  //     category: ProductCategory.drink,
+  //     price: 33000,
+  //     stock: 10,
+  //   ),
+  // ];
 
   @override
   void initState() {
-    searchResults = products;
+    //searchResults = products;
     super.initState();
   }
 
   void onCategoryTap(int index) {
-    searchController.clear();
-    indexValue.value = index;
-    if (index == 0) {
-      searchResults = products;
-    } else {
-      searchResults = products
-          .where((e) => e.category.index.toString().contains(index.toString()))
-          .toList();
-    }
-    setState(() {});
+  // Perbarui nilai indexValue
+  indexValue.value = index;
+
+  // Clear search input
+  searchController.clear();
+
+  String category = 'all'; // Default kategori 'all'
+
+  switch (index) {
+    case 0:
+      category = 'all'; // Gunakan '=' untuk menetapkan nilai
+      break;
+    case 1:
+      category = 'drink';
+      break;
+    case 2:
+      category = 'food';
+      break;
+    case 3:
+      category = 'snack';
+      break;
   }
+
+  // Menambahkan event fetchByKategori dengan kategori yang dipilih
+  context.read<ProductBloc>().add(ProductEvent.fetchByKategori(category));
+}
+
 
   @override
   Widget build(BuildContext context) {
-    if (products.isEmpty) {
-      return const ProductEmpty();
-    }
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -93,12 +107,12 @@ class _HomePageState extends State<HomePage> {
             SearchInput(
               controller: searchController,
               onChanged: (value) {
-                indexValue.value = 0;
-                searchResults = products
-                    .where((e) =>
-                        e.name.toLowerCase().contains(value.toLowerCase()))
-                    .toList();
-                setState(() {});
+                // indexValue.value = 0;
+                // searchResults = products
+                //     .where((e) =>
+                //         e.name.toLowerCase().contains(value.toLowerCase()))
+                //     .toList();
+                // setState(() {});
               },
             ),
             const SpaceHeight(20.0),
@@ -137,27 +151,60 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             const SpaceHeight(35.0),
-            if (searchResults.isEmpty)
-              const Padding(
-                padding: EdgeInsets.only(top: 80.0),
-                child: ProductEmpty(),
-              )
-            else
-              GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: searchResults.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  childAspectRatio: 0.65,
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 30.0,
-                  mainAxisSpacing: 30.0,
-                ),
-                itemBuilder: (context, index) => ProductCard(
-                  data: searchResults[index],
-                  onCartButton: () {},
-                ),
-              ),
+            BlocBuilder<ProductBloc, ProductState>(
+              builder: (context, state) {
+                return state.maybeWhen(
+                  orElse: () {
+                    return SizedBox();
+                  },
+                  loading: () {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                  error: (message) {
+                    return Center(
+                      child: Text(message),
+                    );
+                  },
+                  success: (products) {
+                    if (products.isEmpty) return const ProductEmpty();
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: products.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        childAspectRatio: 0.65,
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 30.0,
+                        mainAxisSpacing: 30.0,
+                      ),
+                      itemBuilder: (context, index) => ProductCard(
+                        data: products[index],
+                        onCartButton: () {},
+                      ),
+                    );
+                  },
+                );
+                // return GridView.builder(
+                //       shrinkWrap: true,
+                //       physics: const NeverScrollableScrollPhysics(),
+                //       itemCount: products.length,
+                //       gridDelegate:
+                //           const SliverGridDelegateWithFixedCrossAxisCount(
+                //         childAspectRatio: 0.65,
+                //         crossAxisCount: 2,
+                //         crossAxisSpacing: 30.0,
+                //         mainAxisSpacing: 30.0,
+                //       ),
+                //       itemBuilder: (context, index) => ProductCard(
+                //         data: products[index],
+                //         onCartButton: () {},
+                //       ),
+                //     );
+              },
+            ),
             const SpaceHeight(30.0),
           ],
         ),
